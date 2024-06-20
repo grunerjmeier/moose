284653f50b50ac598bd5e921e6427757b6b22aa7
mjg <joerg.meier@gruner.ch>
2024-06-20 07:22:34 +0200

    fixing registering of objects and actions

--- framework/src/userobjects/TimedSubdomainModifier.C
+++ framework/src/userobjects/TimedSubdomainModifier.C
@@ -338,68 +338,6 @@ TimedSubdomainModifier::buildFromStages()
   _init_from_stages = false;
 }
 
-void
-TimedSubdomainModifier::buildFromStages()
-{
-  if (_init_from_stages == false)
-    return;
-
-  std::vector<Real> raw_times;
-  std::vector<SubdomainID> raw_from;
-  std::vector<SubdomainID> raw_to;
-
-  // find the stages-object
-  FEProblemBase * fe_problem = this->getParam<FEProblemBase *>("_fe_problem_base");
-  if (fe_problem->hasUserObject("Stages") == false)
-    mooseError("Stages user object not found.");
-  Stages & stgs = fe_problem->getUserObject<Stages>("Stages");
-
-  // iterate all stages
-  std::vector<std::reference_wrapper<Stage>> vecStages = stgs.getStages();
-  int n = vecStages.size();
-  for (int i = 0; i < n; ++i)
-  {
-    Stage & stg = vecStages[i].get();
-
-    // iterate all items of this stage
-    auto items = stg.getItems();
-    int m = items.size();
-    for (int j = 0; j < m; ++j)
-    {
-      // we need only item of type StagedSubdomainModification
-      GeneralUserObject & item = items[j].get();
-
-      if (typeid(item) == typeid(StagedSubdomainModification))
-      {
-        StagedSubdomainModification & smd = dynamic_cast<StagedSubdomainModification &>(item);
-
-        // collect all data of this subdomain modification
-        Real sdTime = stg.getStageTime();
-        std::vector<SubdomainName> sdFromNames = smd.SubdomainsFrom();
-        std::vector<SubdomainName> sdToNames = smd.SubdomainsTo();
-
-        // make SubdomainIDs from SubdomainNames
-        std::vector<SubdomainID> sdFromIDs = _mesh.getSubdomainIDs(sdFromNames);
-        std::vector<SubdomainID> sdToIDs = _mesh.getSubdomainIDs(sdToNames);
-
-        // store the data
-        int n_ids = sdFromIDs.size();
-        for (int k = 0; k < n_ids; ++j)
-        {
-          raw_times.push_back(sdTime);
-          raw_from.push_back(sdFromIDs[k]);
-          raw_to.push_back(sdToIDs[k]);
-        }
-      };
-    };
-  };
-
-  _times = raw_times;
-  _blocks_from = raw_from;
-  _blocks_to = raw_to;
-  _init_from_stages = false;
-}
-
 SubdomainID
 TimedSubdomainModifier::computeSubdomainID()
 {
