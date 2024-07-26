01ebed6a57ba477c37295ca41b5a72faa0eaf91c
Jörg Meier <joerg.meier@gruner.ch>
2024-07-26 14:47:21 +0200

    Stages 2024-07-11

--- framework/src/userobjects/TimedSubdomainModifier.C
+++ framework/src/userobjects/TimedSubdomainModifier.C
@@ -11,6 +11,8 @@
 #include "TimedElementSubdomainModifier.h"
 #include "DelimitedFileReader.h"
 #include "MooseMesh.h"
+#include "Stages.h"
+#include "StagedSubdomainModification.h"
 
 registerMooseObject("MooseApp", TimedSubdomainModifier);
 
@@ -46,6 +48,11 @@ TimedSubdomainModifier::validParams()
       "blocks_to_column_index time_column_text blocks_from_column_text blocks_to_column_text",
       "Subdomain change data from CSV file");
 
+  // parameters for stages-based data supply
+  params.addParam<bool>("from_stages",
+                        "Indicates that the data on times, blocks_from, and blocks_to should be "
+                        "imported from the [Stages] block tree.");
+
   params.addClassDescription(
       "Modify element subdomain ID of entire subdomains for given points "
       "in time. This userobject only runs on the undisplaced mesh, and it will "
@@ -74,13 +81,15 @@ TimedSubdomainModifier::TimedSubdomainModifier(const InputParameters & parameter
   const auto from_parameters =
       isParamSetByUser("times") + isParamSetByUser("blocks_from") + isParamSetByUser("blocks_to");
 
+  const auto from_stages = isParamSetByUser("from_stages");
+
   // Check parameter set for inconsistencies
-  const auto from_source_count = ((from_data_file > 0) + (from_parameters > 0));
+  const auto from_source_count = ((from_data_file > 0) + (from_parameters > 0) + (from_stages > 0));
   if (from_source_count != 1)
   {
     mooseError("Data on times and blocks must be provided either via a CSV file ('data_file' and "
                "corresponding blocks), or via direct parameter input ('times', 'blocks_from', and "
-               "'blocks_to').");
+               "'blocks_to'), or from stages.");
   }
 
   if (from_parameters > 0)
@@ -106,6 +115,10 @@ TimedSubdomainModifier::TimedSubdomainModifier(const InputParameters & parameter
       mooseError("Header flag must be active if columns are to be found via header.");
 
     buildFromFile();
+  }
+  else if (from_stages > 0)
+  {
+    _init_from_stages = true;
   }
   else
     mooseError("Unknown data source. Are you missing a parameter? Did you misspell one?");
@@ -272,9 +285,75 @@ TimedSubdomainModifier::getSubdomainIDAndCheck(const std::string & subdomain_nam
   return id;
 }
 
+void
+TimedSubdomainModifier::buildFromStages()
+{
+  if (_init_from_stages == false)
+    return;
+
+  std::vector<Real> raw_times;
+  std::vector<SubdomainID> raw_from;
+  std::vector<SubdomainID> raw_to;
+
+  // find the stages-object
+  FEProblemBase * fe_problem = this->getParam<FEProblemBase *>("_fe_problem_base");
+  if (fe_problem->hasUserObject("Stages") == false)
+    mooseError("Stages user object not found.");
+  Stages & stgs = fe_problem->getUserObject<Stages>("Stages");
+
+  // iterate all stages
+  std::vector<std::reference_wrapper<Stage>> vecStages = stgs.getStages();
+  int n = vecStages.size();
+  for (int i = 0; i < n; ++i)
+  {
+    Stage & stg = vecStages[i].get();
+
+    // iterate all items of this stage
+    auto items = stg.getItems();
+    int m = items.size();
+    for (int j = 0; j < m; ++j)
+    {
+      // we need only item of type StagedSubdomainModification
+      GeneralUserObject & item = items[j].get();
+
+      if (typeid(item) == typeid(StagedSubdomainModification))
+      {
+        StagedSubdomainModification & smd = dynamic_cast<StagedSubdomainModification &>(item);
+
+        // collect all data of this subdomain modification
+        Real sdTime = stg.getStageTime();
+        std::vector<SubdomainName> sdFromNames = smd.SubdomainsFrom();
+        std::vector<SubdomainName> sdToNames = smd.SubdomainsTo();
+
+        // make SubdomainIDs from SubdomainNames
+        std::vector<SubdomainID> sdFromIDs = _mesh.getSubdomainIDs(sdFromNames);
+        std::vector<SubdomainID> sdToIDs = _mesh.getSubdomainIDs(sdToNames);
+
+        // store the data
+        int n_ids = sdFromIDs.size();
+        for (int k = 0; k < n_ids; ++k)
+        {
+          raw_times.push_back(sdTime);
+          raw_from.push_back(sdFromIDs[k]);
+          raw_to.push_back(sdToIDs[k]);
+        }
+      };
+    };
+  };
+
+  _times = raw_times;
+  _blocks_from = raw_from;
+  _blocks_to = raw_to;
+  _init_from_stages = false;
+}
+
 SubdomainID
 TimedSubdomainModifier::computeSubdomainID()
 {
+  // do we have to build from stages
+  if (_init_from_stages)
+    buildFromStages();
+
   // get the subdomain-id of the current element
   SubdomainID resulting_subdomain_id = _current_elem->subdomain_id();
 
